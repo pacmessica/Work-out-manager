ba861ee709c4fd4ac3218af6df204c728be80c36
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-10 23:23:49 +0200

    add header partial

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "layouts/application.html.erb", type: :view do
+  it "renders a header" do
+    render
+    expect(response).to render_template(:_header)
+  end
+
+  it "displays an alert" do
+    render
+    expect(response).to have_css('p.alert')
+  end
+end
