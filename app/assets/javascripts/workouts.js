1450b4eda914694699b473e69bf7c0fb0d1e8b8b
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-11 20:33:40 +0200

    show only first exercise

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  $(".start-workout-button").on("click", startWorkout);
+$(document).ready (function() {
+  $(".start-workout").on("click", startWorkout);
 });
 
 function startWorkout(){
   $(".workout-overview").hide();
+  $(".workout-animation").show();
+  $(".workout-animation").children('.exercise-view:first').show()
 }
--- spec/features/workout_spec.rb
+++ spec/features/workout_spec.rb
@@ -8,4 +8,16 @@ feature 'Begin Workout Animation', :js => true do
     click_link("Start Workout")
     expect(page).not_to have_css('.workout-overview')
   end
+
+  scenario 'Show Workout Animation' do
+    visit workout_path(workout)
+    click_link("Start Workout")
+    expect(page).to have_css('.workout-animation')
+  end
+
+  scenario 'Show only first exercise' do
+    visit workout_path(workout)
+    click_link("Start Workout")
+    expect(page).to have_css('.exercise-view', count: 1, text: workout.exercises.first.name)
+  end
 end
