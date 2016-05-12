c4eebbc390f421caec8cf6b1de69b6bf2fb7e5ac
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-12 16:12:21 +0200

    start workout animation with a break
    I want to have a countdown before the exercise starts, then
    view the first exercise.

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -1,9 +1,17 @@
-$(document).ready (function() {
+function ready() {
   $(".start-workout").on("click", startWorkout);
-});
+}
+$(document).on("page:load", ready);
+$(document).ready(ready)
 
 function startWorkout(){
   $(".workout-overview").hide();
   $(".workout-animation").show();
-  $(".workout-animation").children('.exercise-view:first').show()
+  var breakTime = this.dataset.workoutinterval * 1000;
+  console.log(breakTime)
+  doBreak();
+}
+
+function doBreak() {
+  $(".rest-countdown").show();
 }
--- spec/features/workout_spec.rb
+++ spec/features/workout_spec.rb
@@ -1,23 +1,32 @@
 require 'rails_helper'
 
+def start_workout()
+  visit workout_path(workout)
+  click_link("Start Workout")
+end
+
 feature 'Begin Workout Animation', :js => true do
   let(:workout) { FactoryGirl.create(:workout_with_exercises)}
 
   scenario 'Hide Workout overview' do
-    visit workout_path(workout)
-    click_link("Start Workout")
+    start_workout()
     expect(page).not_to have_css('.workout-overview')
   end
 
   scenario 'Show Workout Animation' do
-    visit workout_path(workout)
-    click_link("Start Workout")
+    start_workout()
     expect(page).to have_css('.workout-animation')
   end
 
-  scenario 'Show only first exercise' do
-    visit workout_path(workout)
-    click_link("Start Workout")
+  scenario 'Show initial break' do
+    start_workout()
+    expect(page).to have_css('.rest-countdown')
+    expect(page).not_to have_css('.exercise-view')
+  end
+
+  scenario 'Show only first exercise after initial break' do
+    start_workout()
+    sleep workout.exercises_workouts.first.interval + 2
     expect(page).to have_css('.exercise-view', count: 1, text: workout.exercises.first.name)
   end
 end
