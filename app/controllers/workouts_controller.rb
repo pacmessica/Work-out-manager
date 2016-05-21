59bd2f248160f0ee7a8ba2b6f6c49874e27653cd
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-21 19:39:59 +0200

    create new workout method

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -10,4 +10,16 @@ class WorkoutsController < ApplicationController
   def new
     @workout = Workout.new
   end
+
+  def create
+    workout_params = params.require(:workout).permit(:name, :interval, :description)
+    workout_params[:user] = current_user
+    @workout = Workout.new(workout_params)
+    # exercsises_workouts_params = params.require(:exercsises_workouts).permit(:instructions, :time, :exercise_id)
+    if @workout.save
+      render json: { workout: @workout }
+    else
+      render json: { errors: @workout.errors, status: :unprocessable_entity }
+    end
+  end
 end
